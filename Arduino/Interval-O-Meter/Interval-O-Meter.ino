9798462559c87816725f7b6bf3a448f660795d79
Trever Stewart <devmolasses@gmail.com>
2016-08-07 21:21:28 -0500

    Updated code to physical build specs.

--- Arduino/Interval-O-Meter/Interval-O-Meter.ino
+++ Arduino/Interval-O-Meter/Interval-O-Meter.ino
@@ -1,56 +1,61 @@
-int focusPin = 1;
-int shutterPin = 0;
-int delayPin = A3;
-int toFocusPin = 4;
-int modePin = 2;
+int halfShutterPin = 0; //Output pin to control half shutter
+int shutterPin = 1;  //Output pin to control full shutter
+int delayPin = A3;  //Input pin to set the time between photos
+int toHalfShutterPin = 4;  //Input pin to set whether or not to use half shutter
+int modePin = 2;  //Input pin to set the shooting mode
 
-bool toFocus;
+bool toHalfShutter;
 bool mode;
 long delayDuration = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize the digital pin as an output.
-  pinMode(focusPin, OUTPUT);
+  pinMode(halfShutterPin, OUTPUT);
   pinMode(shutterPin, OUTPUT);
   pinMode(delayPin, INPUT);
-  pinMode(toFocusPin, INPUT);
+  pinMode(toHalfShutterPin, INPUT);
   pinMode(modePin, INPUT);
   
-  
+  //Determine the shooting mode
+  mode = (digitalRead(modePin) == HIGH);
   
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // Read the mode and delay duration only once to prevent accidental changes
-  // This also means there can be no intentional changes
-  mode = (digitalRead(modePin) == HIGH);
+  
+
+  //Determine the duration of the delay between  photos
   delayDuration = analogRead(delayPin);
   
   //scale delay for timing between 1 second and 60 seconds
   delayDuration = ((float(delayDuration) / 1023) * (60 - 1) + 1) * 1000;
+
+  // Autofocus operation
+//  toHalfShutter = (digitalRead(toHalfShutterPin) == HIGH);
+//  if(toHalfShutter) halfShutter();
   
-  toFocus = (digitalRead(toFocusPin) == HIGH);
+  // AE lock operation
+  digitalWrite(halfShutterPin, digitalRead(toHalfShutterPin));
   
-  
-  if(toFocus) focus();
   if(mode)
     timeLapse();
   else
     starTrail();
 }
 
-void focus() {
-  digitalWrite(focusPin, HIGH);
-  delay(500);
-  digitalWrite(focusPin, LOW);
-  delay(100);
-}
+// ********Uncomment for autofocus operation***********
+//void halfShutter() {
+//  digitalWrite(halfShutterPin, HIGH);
+//  delay(500);
+//  digitalWrite(halfShutterPin, LOW);
+//  delay(100);
+//}
 
 void timeLapse() {
   digitalWrite(shutterPin, HIGH);
-  delay(100);
+  delay(500);
   digitalWrite(shutterPin, LOW);
   delay(delayDuration);
 }
@@ -59,5 +64,5 @@ void starTrail() {
   digitalWrite(shutterPin, HIGH);
   delay(delayDuration);
   digitalWrite(shutterPin, LOW);
-  delay(100);
+  delay(1000);
 }
